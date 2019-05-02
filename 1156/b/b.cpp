da7cc2e9c4db7ec13cfc57ff5b677f5da15e2b3b
calabash_boy <751188973@qq.com>
2019-05-02 15:20:03 +0800

    update gitignore

---
+++ 1156/b/b.cpp
@@ -0,0 +1,7 @@
+<code>def get_end_remark():
+    return random.choice([
+        "Wish everyone high ratings!",
+        "Good luck!",
+        "Have fun!",
+        "Please, read all the problems!"])
+</code>
